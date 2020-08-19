22f6ee4977629e4aaf4d7acea4bedb7006d6f43f
Dharesh <67889047+Dharesh@users.noreply.github.com>
2020-08-19 16:56:15 +0530

    Update Framework.java

--- src/framework/Framework.java
+++ src/framework/Framework.java
@@ -4,7 +4,7 @@ public class Framework {
 
 	public static void main(String[] args) {
 		System.out.println("design");
-		System.out.println("here are my test scripts");
+		System.out.println("Dharesh here");
 		a
 	}
 }
